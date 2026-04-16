236899c029ce9dec0844d6b017806d52c7b2fa0c
kimberly burton <kj.burton03@gmail.com>
2026-04-16 12:35:28 -0400

    database entered

---
+++ scripts/database.js
@@ -0,0 +1,56 @@
+export const database = {
+    face: [
+        {
+            name: "contour",
+            description: "a compination of highlighter and bronzer",
+            cost: 57.89,
+        },
+        {
+            name: "bronzer",
+            description: "make pale face dark",
+            cost: 29.99
+        },
+        {
+            name: "highlighter",
+            description: "shimmer on girly pop",
+            cost: 34.43
+        }
+
+    ],
+    eyes: [
+        {
+            name: "eye-liner",
+            description: "make your eyes pop",
+            cost: 22,
+        },
+        {
+            name: "mascara - black",
+            description: "dark lash miracle",
+            cost: 25,
+        },
+        {
+            name: "mascara - blue",
+            description: "blue out ",
+            cost: 22,
+        }
+
+    ],
+    tools: [
+        {
+            name: "face roller thingy",
+            description: "guava stick thingy",
+            cost: 13,
+        },
+        {
+            name: "brush set - face",
+            description: "set for success",
+            cost: 27,
+
+        },
+        {
+            name: "lash curler",
+            description: "heated",
+            cost: 16.79
+        }
+    ]
+}
